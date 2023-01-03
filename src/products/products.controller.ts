fb712e606c8371fc40dc284654e98b92cda07ee4
Stanislaw Bidowaniec <20791742+s-bidowaniec@users.noreply.github.com>
2023-01-03 21:40:23 +0100

    Add global interceptor

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,6 +1,9 @@
-import { Controller, Delete, Get, Param, Post, Body } from '@nestjs/common';
+import {Controller, Delete, Get, Param, Post, Body, Put, NotFoundException} from '@nestjs/common';
+import {ParseUUIDPipe} from "@nestjs/common";
 import { ProductsService } from './products.service';
 import { CreateProductDTO } from './dtos/create-product.dto';
+import {UpdateProductDTO} from "./dtos/update-product.dto";
+
 @Controller('products')
 export class ProductsController {
     constructor(private productsService: ProductsService) {}
@@ -9,15 +12,33 @@ export class ProductsController {
         return this.productsService.getAll();
         };
     @Get('/:id')
-    getById(@Param('id') id: string) {
-          return this.productsService.getById(id);
+    getById(@Param('id', new ParseUUIDPipe()) id: string) {
+        const currentProd = this.productsService.getById(id);
+        if (!currentProd) {
+            throw new NotFoundException('Product not found');
+        } else {
+            return currentProd;
+        }
     };
     @Delete('/:id')
-    deleteById(@Param('id') id: string){
-        return this.productsService.deleteById(id);
+    deleteById(@Param('id', new ParseUUIDPipe()) id: string){
+        if (!this.productsService.getById(id)) {
+            throw new NotFoundException('Product not found');
+        } else {
+            return this.productsService.deleteById(id);
+            return { success: true };
+        }
     };
     @Post('/')
     create(@Body() productData: CreateProductDTO) {
-    return this.productsService.create(productData);
-}
+        return this.productsService.create(productData);
+    }
+    @Put('/:id')
+    update(@Param('id', new ParseUUIDPipe()) id: string, @Body() productData: UpdateProductDTO){
+        if (!this.productsService.getById(id))
+            throw new NotFoundException('Product not found');
+
+        this.productsService.updateById(id, productData);
+        return { success: true }
+        }
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -10,19 +10,22 @@ export class ProductsService {
   public getById(id: Product["id"]): Product | null {
     return db.products.find(product => product.id === id);
   };
-  public deleteById(id: Product["id"]): object {
+  public deleteById(id: Product["id"]): void {
     const currentProduct: Product = db.products.find(product => product.id === id);
-    if(currentProduct){
-        const currentIndex: number = db.products.indexOf(currentProduct);
-        db.products.splice(currentIndex, 1);
-            return { success: true };
-        } else {
-            return { success: false };
-        }
+    const currentIndex: number = db.products.indexOf(currentProduct);
+    db.products.splice(currentIndex, 1);
   };
   public create(productData: Omit<Product, 'id'>): Product {
     const newProduct = { ...productData, id: uuidv4() };
     db.products.push(newProduct);
     return newProduct;
   };
+  public updateById(id: Product['id'], productData: Omit<Product, 'id'>): void {
+    db.products = db.products.map((p) => {
+        if (p.id === id) {
+            return { ...p, ...productData };
+        }
+        return p;
+    });
+  }
 }
